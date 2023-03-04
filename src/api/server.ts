1e6fea3d4030a54e5431a8a3cf78cd0831135a16
Nathan Iszlaub <nathan.iszlaub@gmail.com>
2023-03-04 12:42:09 +0800

    Add express to project

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,2 +1,6 @@
-import path from "path";
-console.log(path.resolve(__dirname, "src"));
+import express from "express";
+import "dotenv";
+
+const server = express();
+server.get("/", (req, res) => res.status(200).send(`Hello "${req.hostname}"`));
+server.listen(Number(process.env.PORT ?? 3000));
